5e66b2221262df426ea069441b3a9c4542792aa3
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-06-21 13:41:26 +0200

    create auth screens
    - created sign-in and sign-up screens (initial UI) inside another routing group (auth);
    - updated root layout according to the newly created routes;

---
+++ src/app/(auth)/_layout.tsx
@@ -0,0 +1,6 @@
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+import { Stack } from "expo-router";
+
+export default function AuthLayout() {
+  return <Stack />;
+}
---
+++ src/app/(auth)/sign-in.tsx
@@ -0,0 +1,110 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Link, Stack } from "expo-router";
+import { useState } from "react";
+
+import Button from "@/components/ui/Button";
+import Colors from "@/constants/Colors";
+
+const SignInScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState("");
+
+  const resetFields = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const validateInput = () => {
+    setErrors("");
+
+    if (!email) {
+      setErrors("Name is required");
+      return false;
+    }
+
+    if (!password) {
+      setErrors("Price is required");
+      return false;
+    }
+
+    if (password.trim().length < 6) {
+      setErrors("Password has to be at least 6 characters");
+      return false;
+    }
+
+    return true;
+  };
+
+  const onSubmit = () => {
+    if (!validateInput()) return;
+
+    console.warn("Signing in...");
+
+    resetFields();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: "Sign in" }} />
+
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+        placeholder="johndoe@gmail.com"
+      />
+
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+
+      <Text style={styles.errors}>{errors}</Text>
+      <Button text="Sign in" onPress={onSubmit} />
+      <Link href="/sign-up" style={styles.textButton}>
+        Create an account
+      </Link>
+    </View>
+  );
+};
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+
+  label: {
+    color: "gray",
+    fontSize: 16,
+  },
+
+  input: {
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 20,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "gray",
+    backgroundColor: "white",
+  },
+
+  errors: {
+    color: "red",
+  },
+
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
+});
